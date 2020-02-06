3b7eaf365784c8f970991a0e06c1d15e37222084
Andrey Oskin <a_oskin@wargaming.net>
2020-02-06 16:55:41 +0200

    initial

--- src/CompressHashDisplace.jl
+++ src/CompressHashDisplace.jl
@@ -1,5 +1,20 @@
 module CompressHashDisplace
 
-greet() = print("Hello World!")
+# Based on http://stevehanov.ca/blog/?id=119
+
+const DICT = "/usr/share/dict/words"
+
+# Calculates a distinct hash function for a given string. Each value of the
+# integer d results in a different hash value.
+function fnv_hash(s, d::UInt32 = 0x811c9dc5)
+
+    # Use the FNV algorithm from http://isthe.com/chongo/tech/comp/fnv/
+    for c in s
+        d *= 0x01000193
+        d = d ⊻ c
+    end
+
+    return d
+end
 
 end # module
