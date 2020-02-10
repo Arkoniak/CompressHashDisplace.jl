287ade36a404a36b519389553a8adfcc07f34a1d
Andrey Oskin <a_oskin@wargaming.net>
2020-02-10 17:08:21 +0200

    Improved version of frozen dictionary with MurmurHash3

--- src/CompressHashDisplace.jl
+++ src/CompressHashDisplace.jl
@@ -1,88 +1,105 @@
 module CompressHashDisplace
 
+using MurmurHash3
 include("hash.jl")
-export create_minimal_perfect_hash, perfect_hash_lookup
+export FrozenDict, lookup
+
+mmhash(str::String) = last(mmhash128_a(sizeof(str), pointer(str), 0%UInt32))
+mmhash(str::String, d::UInt32) = last(mmhash128_a(sizeof(str), pointer(str), d))
+
+struct FrozenDict{V}
+    G::Vector{Int}
+    values::Vector{V}
+    sz::UInt64
+end
+
+optimal_size(dict) = 1 << (leading_zeros(0) - leading_zeros(length(dict)))
 
 # Computes a minimal perfect hash table using the given python dictionary. It
 # returns a tuple (G, V). G and V are both arrays. G contains the intermediate
 # table of values needed to compute the index of the value in V. V contains the
 # values of the dictionary.
-function create_minimal_perfect_hash(dict::Dict{K, V}, sz = 2^17) where {K, V}
-    szmask = UInt64(sz - 1)
+function FrozenDict(dict::Dict{K, V}) where {K, V}
+    sz = optimal_size(dict)
+    szmask = (sz - 1) % UInt64
 
     # Step 1: Place all of the keys into buckets
-    buckets = [ K[] for i in 1:sz ]
+    buckets = [K[] for _ in 1:sz]
     G = zeros(Int, sz)
-    values = Vector{Union{Int, Nothing}}(undef, sz)
+    # values = Vector{Union{Int, Nothing}}(undef, sz)
+    values = Vector{V}(undef, sz)
+    keyset = BitArray(undef, sz)
+    for i in 1:sz
+        keyset[i] = false
+    end
 
     for key in keys(dict)
-        push!(buckets[hash(key, UInt64(0)) & szmask + 1], key)
+        idx = mmhash(key, 0 % UInt32) & szmask + 1
+        push!(buckets[idx], key)
     end
 
     # Step 2: Sort the buckets and process the ones with the most items first.
     sort!(buckets, by = length, rev = true)
-    for b in 1:sz
+    slots = Vector{Int}()
+    sizehint!(slots, length(first(buckets)))
+    b = 1
+    while b <= sz
         bucket = buckets[b]
         length(bucket) <= 1 && break
 
-        d = UInt64(1)
+        d = 1 % UInt32
         item = 1
-        slots = []
 
         # Repeatedly try different values of d until we find a hash function
         # that places all items in the bucket into free slots
-
         while item <= length(bucket)
-            slot = hash(bucket[item], d) & szmask + 1
-            if (values[slot] != nothing) || (slot in slots)
-                d += UInt64(1)
+            slot = mmhash(bucket[item], d) & szmask + 1
+            if keyset[slot] || (slot in slots)
+                d += 1 % UInt32
                 item = 1
-                slots = []
+                empty!(slots)
             else
                 push!(slots, slot)
                 item += 1
             end
         end
 
-        G[hash(bucket[1], UInt64(0)) & szmask + 1] = d
+        # We already know this value, why do we calculate it again??
+        G[mmhash(first(bucket), 0 % UInt32) & szmask + 1] = d
         for i in 1:length(bucket)
             values[slots[i]] = dict[bucket[i]]
+            keyset[slots[i]] = true
         end
+        empty!(slots)
+        b += 1
     end
 
-    # Only buckets with 1 item remain. Process them more quickly by directly
-    # placing them into a free slot. Use a negative value of d to indicate
-    # this.
-    freelist = []
-    for i in 1:sz
-        values[i] == nothing && push!(freelist, i)
+    idx = 1
+    for b2 in b:sz
+        bucket = buckets[b2]
+        isempty(bucket) && break
+        bucket = first(bucket)
+        while keyset[idx]
+            idx += 1
+        end
+
+        # why we calculate it second time?? It makes no sense at all
+        G[mmhash(bucket, 0 % UInt32) & szmask + 1] = -idx
+        values[idx] = dict[bucket]
+        idx += 1
     end
 
-    for b in 1:sz
-        bucket = buckets[b]
-        length(bucket) != 1 && continue
-        slot = pop!(freelist)
-        G[hash(bucket[1], UInt64(0)) & szmask + 1] = -slot
-        values[slot] = dict[bucket[1]]
-    end
-
-    vals = Vector{Int}(undef, sz)
-    for i in 1:sz
-        values[i] == nothing && continue
-        vals[i] = values[i]
-    end
-
-    return G, vals
+    return FrozenDict{V}(G, values, sz)
 end
 
 # Look up a value in the hash table, defined by G and V.
-function perfect_hash_lookup(G, V, key)
-    szmask = UInt64(2^17 - 1)
-    @inbounds d = G[hash(key, UInt64(0)) & szmask + UInt64(1)]
+function lookup(FD::FrozenDict{V}, key) where V
+    szmask = UInt64(FD.sz - 1)
+    @inbounds d = FD.G[mmhash(key, 0%UInt32) & szmask + 1%UInt32]
     if d < 0
-        @inbounds return V[-d]
+        @inbounds return FD.values[-d]
     else
-        @inbounds return V[hash(key, d % UInt64) & szmask + UInt64(1)]
+        @inbounds return FD.values[mmhash(key, d % UInt32) & szmask + 1%UInt32]
     end
 end
 
