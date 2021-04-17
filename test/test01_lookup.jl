2310770c38a54b849ada91fa127a65749b4c4531
Andrey Oskin <skoffer@arkoniak.com>
2021-04-17 16:25:26 +0300

    bump version

--- test/test01_lookup.jl
+++ test/test01_lookup.jl
@@ -14,4 +14,16 @@ using Test
     @test fd["world"] == 2
 end
 
+@testset "Nonstring Lookup" begin
+    dict = Dict{String, Int}(5 => 1, 6 => 2)
+    fd = FrozenDict(dict)
+    @test fd[5] == 1
+    @test fd[6] == 2
+    @test_throws KeyError fd["goodbye"]
+
+    fud = FrozenUnsafeDict(dict)
+    @test fd[5] == 1
+    @test fd[6] == 2
+end
+
 end # module
